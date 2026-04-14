8159406312c96d96bea1667d76b278a576442da7
K-wakaT <hasukiiq@gmail.com>
2026-04-14 15:05:30 +0900

    initial: Nix environment setup

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Esslens development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.python312
+            pkgs.python312Packages.pip
+            pkgs.gh
+            pkgs.git
+            pkgs.nodejs_20
+          ];
+        };
+      });
+}
